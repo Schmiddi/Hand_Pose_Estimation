1f307594c5df3bd064e83ee9d090dc3c399659b0
karkuspeter <karkuspeter@gmail.com>
2015-10-05 19:18:33 +0800

    Code to compute information gain

---
+++ info_gain.py
@@ -0,0 +1,26 @@
+import sys
+import glob
+import os
+import numpy as np
+
+
+def info_gain(S, Sl, Sr):
+    """S is a the set of structure examples. Sl is the subset that
+       goes to the left, Sr is the subset that goest to the right
+       based on the split criteria at this node
+
+       S - numpy array, rows are variables, columns are samples
+    """
+    #S is useless, S = Sl + Sr
+
+    infgain = entropy(S) - ( np.shape(Sl)[1] / np.shape(S)[1] * entropy(Sl) + np.shape(Sr)[1] / np.size(S) * entropy(Sr))
+    return infgain
+
+def entropy(S):
+    """ computes the entropy for S
+    """
+    H = 0.5 * np.log(np.linalg.det(covariant(S))) + 0.5 * np.shape(S)[1] * (1 + 1.83787706641) # ln(2pi) = 1.8378..
+    return H
+
+def covariant(S):
+    return np.cov(S)
