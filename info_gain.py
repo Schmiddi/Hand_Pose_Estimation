eb5f2aac41fc3a8cfd8d290f907b38aaa8cfd20f
Dennis Schmidt <schmiddi11@gmx.de>
2015-11-01 21:02:56 +0800

    Tweat random forest (without success)
    - Change input: row=sample, col=label/featrue
    - Optimize the selection of split thresholds

--- info_gain.py
+++ info_gain.py
@@ -15,7 +15,7 @@ def info_gain(S, Sl, Sr):
     """
     #S is useless, S = Sl + Sr
 
-    infgain = entropy(S) - ( np.shape(Sl)[1] / float(np.shape(S)[1]) * entropy(Sl) + np.shape(Sr)[1] / float(np.shape(S)[1]) * entropy(Sr))
+    infgain = entropy(S) - ( np.shape(Sl)[0] / float(np.shape(S)[0]) * entropy(Sl) + np.shape(Sr)[0] / float(np.shape(S)[0]) * entropy(Sr))
     return infgain
 
 def entropy(S):
@@ -24,20 +24,25 @@ def entropy(S):
     #pdb.set_trace()
     #warnings.simplefilter("error", RuntimeWarning)
     det = np.linalg.det(covariant(S))
-    if det == 0:
-        det = 1e-20
+    if det==0:
+        det = 1e-7
+         
+    #log = 0 if det == 0 else np.log(det)
+    log = np.log(det)
+    # Maybe the zero values causing some problems, try something big ;)
     #try:
-    H = 0.5 * np.log(det) + 0.5 * np.shape(S)[1] * (1 + 1.83787706641) # ln(2pi) = 1.8378..
+    H = 0.5 * log + 0.5 * np.shape(S)[0] * (1 + 1.83787706641) # ln(2pi) = 1.8378..
     #except RuntimeWarning:
     #    print "Warning"
     return H
 
 def covariant(S):
-    if S.size==0:
-        covariance = np.zeros([1,1])
-    elif S.size == 1:
-        covariance = np.matrix(S)
-    else:
-        covariance = np.matrix(np.cov(S, rowvar=1)) 
+    if S.shape[0]==0:
+        return np.zeros([1,1])
+    
+    if S.shape[0] == 1:
+        return np.zeros([S.shape[1],S.shape[1]])
+    
+    covariance = np.matrix(np.cov(S,rowvar=0))
         
     return covariance
--- random_forest.py
+++ random_forest.py
@@ -3,46 +3,41 @@ import numpy as np
 import info_gain
 
 def test_tree():
-    n_samples = 100
+    n_samples = 5000
     n_features = 27
     new_array = []
     random.seed(1)
-    n_labels = np.zeros([1,n_samples])
-    new_array = np.zeros([n_features, n_samples])
+    n_labels = np.zeros([n_samples,1])
+    new_array = np.zeros([n_samples, n_features])
     
     numLabels = 3
     
     for i in xrange(n_samples):
-        n_labels[0,i] = random.randint(1,numLabels) #1,2,3
+        n_labels[i,0] = random.randint(1,numLabels) #1,2,3
         for j in xrange(n_features):
-            new_array[j,i] = np.random.uniform((n_labels[0,i]-1)*1/numLabels,n_labels[0,i]*1/numLabels)
+            new_array[i,j] = np.random.uniform((n_labels[i,0]-1)*1/numLabels,n_labels[i,0]*1/numLabels)
         
 
-    forest = create_forest(new_array, n_labels,10)
-    print forest
-    print classify_w_forest(forest, new_array[:,0]),n_labels[0,0]
-    print classify_w_forest(forest, new_array[:,1]),n_labels[0,1]
-    print classify_w_forest(forest, new_array[:,2]),n_labels[0,2]
-    print classify_w_forest(forest, new_array[:,3]),n_labels[0,3]
-    print classify_w_forest(forest, new_array[:,4]),n_labels[0,4]
-    print classify_w_forest(forest, new_array[:,5]),n_labels[0,5]
-    print classify_w_forest(forest, new_array[:,6]),n_labels[0,6]
+    forest = create_forest(new_array, n_labels,1)
+    #print forest
+    for i in xrange(20):
+        print classify_w_forest(forest, new_array[i,:]),n_labels[i,0]
     print "hello world"
 
 '''
-    D_n: rows = features, col = samples (Feature vector: x = [0,1]^d)
-    D_n_labels: row=labels, col = samples (Label vector: y =  R^q)
+    D_n: rows = samples, col = features (Feature vector: x = [0,1]^d)
+    D_n_labels: row=samples, col = labels (Label vector: y =  R^q)
     num_trees: number of trees to generate (default: 10)
-    t_s: step size for the thresholds (default: 0.01)
+    steps: step size for the thresholds (default: 100)
     m_0: amount of structure data we maximal should use for the next construction step (default: 1e7)
     k_n: minimum amount of estimations examples per node (default: 30)
     p: predefined probability for feature selection (default: 0.2)
 '''
-def create_forest(D_n, D_n_labels, num_trees=10, t_s = 0.01, k_n=30, m_0=1e7, p=0.2):
+def create_forest(D_n, D_n_labels, num_trees=10, steps = 100, k_n=30, m_0=1e7, p=0.2):
     forest = []
     for i in xrange(num_trees):
         print "Creating tree %d\r"%i
-        forest.append(create_tree (D_n, D_n_labels, t_s, k_n, m_0, p))
+        forest.append(create_tree (D_n, D_n_labels, steps, k_n, m_0, p))
     
     return forest
 
@@ -62,8 +57,8 @@ def classify_w_tree(tree, sample):
         if tree_node[i][0] is None:
             # Yeah it's a leave node, we can finally predict!!
             e_labels = estimation_node[tree_node[i][1]]
-            # Average each row of the estimation data
-            prediction = np.sum(e_labels, axis=1)/e_labels.shape[1]
+            # Average each col of the estimation data
+            prediction = np.sum(e_labels, axis=0)/e_labels.shape[0]
             return prediction
         
         # else - non leave node
@@ -74,88 +69,95 @@ def classify_w_tree(tree, sample):
         
     
 '''
-    D_n: rows = features, col = samples (Feature vector: x = [0,1]^d)
-    D_n_labels: row=labels, col = samples (Label vector: y =  R^q)
-    t_s: step size for the thresholds (default: 0.01)
+    D_n: rows = samples, col = features (Feature vector: x = [0,1]^d)
+    D_n_labels: row=samples, col = labels (Label vector: y =  R^q)
+    steps: step size for the thresholds (default: 100)
     m_0: amount of structure data we maximal should use for the next construction step (default: 1e7)
     k_n: minimum amount of estimations examples per node (default: 30)
     p: predefined probability for feature selection (default: 0.2)
 '''
-def create_tree (D_n, D_n_labels, t_s = 0.01, k_n=30, m_0=1e7, p=0.2):
+def create_tree (D_n, D_n_labels, steps = 100, k_n=30, m_0=1e7, p=0.2):
     # Split the data set D_n into two parts
     # Put floor(n/2) random samples into U_n,
     # and the remaining ceil(n/2) samples in E_n 
-    random_index = random.sample(xrange(D_n.shape[1]), D_n.shape[1] / 2)
-    n = range(0, D_n.shape[1])
+    random_index = random.sample(xrange(D_n.shape[0]), D_n.shape[0] / 2)
+    n = range(0, D_n.shape[0])
     ## Structure data
-    U_n = D_n[ :, random_index]
-    U_n_labels = D_n_labels[ :, random_index]
+    U_n = D_n[random_index, :]
+    if(len(D_n_labels.shape) == 1):
+        D_n_labels = D_n_labels.reshape(D_n_labels.shape[0],1)
+        
+    U_n_labels = D_n_labels[random_index, :]
     ## Estimation data
-    E_n = D_n[ :, list(set(n) - set(random_index))]
-    E_n_labels = D_n_labels[ :, list(set(n) - set(random_index))]
+    E_n = D_n[ list(set(n) - set(random_index)),:]
+    E_n_labels = D_n_labels[ list(set(n) - set(random_index)),:]
     tree_node = []
     estimation_node = []
     # Set the maximal height of a tree
-    max_h = np.log2(D_n.shape[1])
-    return create_node(U_n, U_n_labels, E_n, E_n_labels, 0, max_h, tree_node, estimation_node, t_s, k_n, m_0, p)
+    max_h = np.log2(D_n.shape[0])
+    return create_node(U_n, U_n_labels, E_n, E_n_labels, 0, max_h, tree_node, estimation_node, steps, k_n, m_0, p)
 
 
 '''
-    t_s: step size for the thresholds (default: 0.01)
+    steps: step size for the thresholds (default: 100)
     m_0: amount of structure data we maximal should use for the next construction step (default: 1e7)
     k_n: minimum amount of estimations examples per node (default: 30)
     p: predefined probability for feature selection (default: 0.2)
 '''
-def create_node(U_n, U_n_labels, E_n, E_n_labels, h, max_h, tree_node, estimation_node, t_s = 0.01, k_n=30, m_0=1e7, p=0.2):
+def create_node(U_n, U_n_labels, E_n, E_n_labels, h, max_h, tree_node, estimation_node, steps = 100, k_n=30, m_0=1e7, p=0.2):
     # Define tuning parameters
     ## Define candidate thresholds from 0 to 1 in 0.1 steps
-    T = np.arange(0, 1, t_s)
+    #T = np.arange(0, 1, steps)
     ## Number of distinct features we select for tree construction
-    s = 1 + np.random.binomial(U_n.shape[0], p)
+    s = 1 + np.random.binomial(U_n.shape[1], p)
     ## Get s random indices
-    s_index = random.sample(xrange(U_n.shape[0]), s)
+    s_index = random.sample(xrange(U_n.shape[1]), s)
     ## Calculate the number of structure data to use for the next construction step
-    mu = min(m_0, U_n.shape[1])
+    mu = min(m_0, U_n.shape[0])
     max_t_index = []
     max_value = []
     # Iterate of all feature indices
     for fi in s_index:
         # Get random indices of the structure data (for each feature different data)
-        mu_index = random.sample(xrange(U_n.shape[1]), mu)
+        mu_index = random.sample(xrange(U_n.shape[0]), mu)
         gains = []
+        tmin = np.min(U_n[:,fi])
+        tmax = np.max(U_n[:,fi])
+        trange = tmax-tmin
+        T = np.arange(tmin,tmax,trange/steps)
         for t in T:
             # Split the M structure data samples based on the threshold t
-            U_1_labels = U_n_labels[:, U_n[fi, mu_index] < t]
-            U_2_labels = U_n_labels[:, U_n[fi, mu_index] >= t]
+            U_1_labels = U_n_labels[U_n[mu_index,fi] < t,:]
+            U_2_labels = U_n_labels[U_n[mu_index, fi] >= t, :]
             # Calculate the information gain using this split
             if U_1_labels.size == 0 or U_2_labels.size ==0:
                 gains.append(0)
             else:
-                gains.append(info_gain.info_gain(U_n_labels[:,mu_index], U_1_labels, U_2_labels))
+                gains.append(info_gain.info_gain(U_n_labels[mu_index,:], U_1_labels, U_2_labels))
         
         # Get the index of the threshold maximizing the info gain, and the info gain value 
         index = np.argmax(np.array(gains))
-        max_t_index.append(index)
+        max_t_index.append(T[index])
         max_value.append(gains[index])
     
     # Get the feature and threshold which maximize the info gain
     index = np.argmax(np.array(max_value))
     max_feature = s_index[index]
-    max_t = T[max_t_index[index]]
+    max_t = max_t_index[index]
 
     # Split the structure and estimation data based on the best feature and threshold
-    U_1 = U_n[:, U_n[max_feature, :] < max_t]
-    U_1_labels = U_n_labels[:, U_n[max_feature, :] < max_t]
-    U_2 = U_n[:, U_n[max_feature, :] >= max_t]
-    U_2_labels = U_n_labels[:, U_n[max_feature, :] >= max_t]
-    E_1 = E_n[:, E_n[max_feature, :] < max_t]
-    E_1_labels = E_n_labels[:, E_n[max_feature, :] < max_t]
-    E_2 = E_n[:, E_n[max_feature, :] >= max_t]
-    E_2_labels = E_n_labels[:, E_n[max_feature, :] >= max_t]
+    U_1 = U_n[U_n[:, max_feature] < max_t, :]
+    U_1_labels = U_n_labels[U_n[:, max_feature] < max_t,:]
+    U_2 = U_n[U_n[:, max_feature] >= max_t, :]
+    U_2_labels = U_n_labels[U_n[:, max_feature] >= max_t,:]
+    E_1 = E_n[E_n[:, max_feature] < max_t, :]
+    E_1_labels = E_n_labels[E_n[:, max_feature] < max_t,:]
+    E_2 = E_n[E_n[:, max_feature] >= max_t, :]
+    E_2_labels = E_n_labels[E_n[:, max_feature] >= max_t,:]
     h += 1
     
     # Check is after the split the tree is still valid
-    if E_1.shape[1] < k_n or E_2.shape[1] < k_n or h > max_h:
+    if E_1.shape[0] < k_n or E_2.shape[0] < k_n or h > max_h:
         # Create new end node
         estimation_node.append(E_n_labels)
         # If it is a leave node, we place the index of the estimation node array holding the estimation nodes of this leave in the second field, the rest None
@@ -169,9 +171,9 @@ def create_node(U_n, U_n_labels, E_n, E_n_labels, h, max_h, tree_node, estimatio
     # Node [ best feature, best threshold, index left node, index right node]
     node = [max_feature, max_t, index_node + 1, None]
     tree_node.append(node)
-    tree_node, estimation_node = create_node(U_1, U_1_labels, E_1, E_1_labels, h, max_h, tree_node, estimation_node, t_s, k_n, m_0, p)
+    tree_node, estimation_node = create_node(U_1, U_1_labels, E_1, E_1_labels, h, max_h, tree_node, estimation_node, steps, k_n, m_0, p)
     tree_node[index_node][3] = len(tree_node)
-    tree_node, estimation_node = create_node(U_2, U_2_labels, E_2, E_2_labels, h, max_h, tree_node, estimation_node, t_s, k_n, m_0, p)
+    tree_node, estimation_node = create_node(U_2, U_2_labels, E_2, E_2_labels, h, max_h, tree_node, estimation_node, steps, k_n, m_0, p)
 
     return tree_node, estimation_node
 
