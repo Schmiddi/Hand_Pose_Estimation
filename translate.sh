56b2037fca6bf7f3c7486c68f660868addf837bf
Ramesh Rajaby <ramesh.rajaby@gmail.com>
2015-11-06 20:42:05 +0800

    Added script to randomly translate hands positions

---
+++ translate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for i in {0..5}; do
+for f in $1/*.d.png; do
+	echo $f
+	convert $f -trim +repage trimmed.png
+	dim=`convert trimmed.png info:- | cut -d" " -f 3`
+	w=`echo $dim | cut -d"x" -f 1`
+	h=`echo $dim | cut -d"x" -f 2`
+	lpos=$(( $RANDOM % (160-$w) ))
+	upos=$(( $RANDOM % (160-$h) ))
+	rm trimmed.png
+
+	convert $f -gravity center -background 'gray(1.14443%,1.14443%,1.14443%)' -extent 320x320 +repage extended.png
+	lcrop=$(( 160-(w/2)-lpos ))
+	ucrop=$(( 160-(h/2)-upos ))
+	geometry="160x160+"$lcrop"+"$ucrop
+	fname="`basename $f | cut -d"." -f 1`-$lpos-$upos.d.png"
+	convert extended.png -crop $geometry +repage "$2/$fname"
+	rm extended.png
+done
+done
