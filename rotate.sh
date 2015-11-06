8b25d85bba3428c81b0504c23c1b85ae485940c0
Ramesh Rajaby <ramesh.rajaby@gmail.com>
2015-11-06 18:53:34 +0800

    Added repaging after rotation

--- rotate.sh
+++ rotate.sh
@@ -4,6 +4,6 @@ for f in $1/*.d.png; do
 	echo $f
 	fname=`basename $f | cut -d"." -f 1`
 	for angle in $(eval echo {0..359..$3}); do
-		convert $f -rotate $angle +repage -gravity center -crop 160x160+0+0 "$2/$fname-$angle.d.png"
+		convert $f -rotate $angle -gravity center -crop 160x160+0+0 +repage "$2/$fname-$angle.d.png"
 	done
 done
